3452b959064c5238d98fafba96107c630b8c27de
Jah-On <58399643+Jah-On@users.noreply.github.com>
2021-11-27 22:46:51 -0600

    Add files via upload

---
+++ testing.cpp
@@ -0,0 +1,10 @@
+#include "Cimulate.h"
+
+int main(){
+    Cimulate sim = Cimulate();
+    sim.keyboard.toggle(sim.keyboard._0, 1);
+    sim.keyboard.toggle(sim.keyboard._0, 0);
+    sim.mouse.setPos(0,0);
+    sim.mouse.toggle(sim.mouse._LEFT, 1);
+    sim.mouse.toggle(sim.mouse._LEFT, 0);
+}
